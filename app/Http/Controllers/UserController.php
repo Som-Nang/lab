812bb9174f2c79d33307faecf0c8e8ab4edcaeaf
Som-Nang <somnangmega13@gmail.com>
2024-01-19 17:02:26 +0700

    login logic

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,14 +3,41 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Auth;
 
 class UserController extends Controller
 {
-   public function user()
-   {
-       return[
-        'name' => 'jonh',
-        'state' => 'active'
-       ];
-   }
+    public function login()
+    {
+        return inertia('User/Login');
+    }
+
+    public function verifyLogin(Request $request)
+    {
+
+        $request->validate([
+            'email' => 'required|email',
+            'password' => 'required',
+            'remember' => 'nullable|boolean'
+        ]);
+
+        $user = User::where('email', $request->email)->first();
+        if ($user) {
+            $encryptedPassword = $user->password;
+            $checkPassword = Hash::check($request->password, $encryptedPassword);
+            if ($checkPassword) {
+                Auth::login($user, $request->remember);
+                $request->session()->regenerate();
+
+                return redirect()->to('/dashboard');
+            } else {
+
+                return back()->withErrors([
+                    'email' => 'The provided credentials do not match our records.',
+                ]);
+            }
+        }
+    }
 }
